c3c0dad4a937469a68a73b9c882535386c41ca91
Dion Brown <dion.brown@outlook.co.nz>
2022-02-26 12:42:11 +1300

    Refactored the imports.

--- lib/blog-pipeline-stack.ts
+++ lib/blog-pipeline-stack.ts
@@ -1,6 +1,18 @@
-import { Stack, StackProps, aws_codepipeline as codepipeline, aws_codepipeline_actions as actions, aws_codebuild as codebuild, aws_s3 } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Pipeline, Artifact } from "aws-cdk-lib/aws-codepipeline";
+import {
+  CodeStarConnectionsSourceAction,
+  CodeBuildAction,
+  S3DeployAction,
+} from "aws-cdk-lib/aws-codepipeline-actions";
+import { Bucket } from "aws-cdk-lib/aws-s3";
+import {
+  PipelineProject,
+  LinuxBuildImage,
+  ComputeType,
+  BuildSpec,
+} from "aws-cdk-lib/aws-codebuild";
+import { Construct } from "constructs";
 
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -8,48 +20,52 @@ export class BlogPipelineStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const pipeline = new codepipeline.Pipeline(this, 'Pipeline', {
-      pipelineName : 'blog-deployment'
+    const pipeline = new Pipeline(this, "Pipeline", {
+      pipelineName: "blog-deployment",
     });
-    const sourceOutput = new codepipeline.Artifact("source")
-    const buildOutput = new codepipeline.Artifact('build');
+    const sourceOutput = new Artifact("source");
+    const buildOutput = new Artifact("build");
 
-    const sourceStage = pipeline.addStage({stageName: 'Source'})
-    
+    const sourceStage = pipeline.addStage({ stageName: "Source" });
 
-    sourceStage.addAction(new actions.CodeStarConnectionsSourceAction({
-      actionName : 'GitHub',
-      connectionArn : `arn:aws:codestar-connections:${this.region}:${this.account}:connection/f6d21f24-21db-4453-8c18-86ed3e230da6`,
-      owner: 'DionBrNz',
-      branch: "master",
-      repo: 'blog',
-      output: sourceOutput
-    }))
+    sourceStage.addAction(
+      new CodeStarConnectionsSourceAction({
+        actionName: "GitHub",
+        connectionArn: `arn:aws:codestar-connections:${this.region}:${this.account}:connection/f6d21f24-21db-4453-8c18-86ed3e230da6`,
+        owner: "DionBrNz",
+        branch: "master",
+        repo: "blog",
+        output: sourceOutput,
+      })
+    );
 
-    const project = new codebuild.PipelineProject(this, 'blog-deployment', {
-      environment:  {
-        buildImage: codebuild.LinuxBuildImage.AMAZON_LINUX_2_ARM_2,
-        computeType: codebuild.ComputeType.SMALL
+    const project = new PipelineProject(this, "blog-deployment", {
+      environment: {
+        buildImage: LinuxBuildImage.AMAZON_LINUX_2_ARM_2,
+        computeType: ComputeType.SMALL,
       },
-      buildSpec: codebuild.BuildSpec.fromSourceFilename('./buildspec.yml'),
-      projectName: 'blog-deployment'
-     })
+      buildSpec: BuildSpec.fromSourceFilename("./buildspec.yml"),
+      projectName: "blog-deployment",
+    });
 
-    const buildStage = pipeline.addStage({stageName: 'Build'})
-    buildStage.addAction(new actions.CodeBuildAction({
-      actionName: 'Build',
-      input: sourceOutput,
-      project,
-      outputs: [buildOutput]
-    }))
+    const buildStage = pipeline.addStage({ stageName: "Build" });
+    buildStage.addAction(
+      new CodeBuildAction({
+        actionName: "Build",
+        input: sourceOutput,
+        project,
+        outputs: [buildOutput],
+      })
+    );
 
-    const deployS3 = pipeline.addStage({stageName: 'Deploy'})
-    deployS3.addAction(new actions.S3DeployAction({
-      bucket: aws_s3.Bucket.fromBucketName(this, 'bucket', 'todothinkofname.net'),
-      actionName: 'Deploy',
-      input: buildOutput,
-      extract: true
-    }))
-
+    const deployS3 = pipeline.addStage({ stageName: "Deploy" });
+    deployS3.addAction(
+      new S3DeployAction({
+        bucket: Bucket.fromBucketName(this, "bucket", "todothinkofname.net"),
+        actionName: "Deploy",
+        input: buildOutput,
+        extract: true,
+      })
+    );
   }
 }
